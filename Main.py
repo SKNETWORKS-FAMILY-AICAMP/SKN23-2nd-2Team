7526c577999b4196f05f55b3f3d08bae56ce9f9f
정유선 <acft9609@gmail.com>
2026-01-11 20:27:19 +0900

    전역 스타일 및 테마 설정

--- Main.py
+++ Main.py
@@ -18,8 +18,31 @@ pages.run()
 
 
 # ======== 스타일 설정 ========
+def load_font(font_path, font_name):
+    with open(font_path, "rb") as f:
+        data = f.read()
+    
+    base64_font = base64.b64encode(data).decode()
+
+    font_style = f"""
+        <style>
+        @font-face {{
+            font-family: {font_name};
+            src: url(data:font/woff2;base64,{base64_font}) format('woff2');
+        }}
+        </style>
+    """
+    st.markdown(font_style, unsafe_allow_html=True)
+
+load_font("assets/fonts/PretendardVariable.woff2", "Pretendard")
+load_font("assets/fonts/NanumSquareNeo-Variable.woff2", "NanumSquareNeo")
+
 st.markdown("""
     <style>
+        html, body, .stMarkdown, p, h1, h2, h3, h4, h5, h6, label, table, div, span, b, [class*="st-"]:not([data-testid="stIconMaterial"]):not(span) {
+            font-family: "Pretendard", "NanumSquareNeo", "Source Sans", sans-serif;
+        }
+            
         [data-testid="stSidebarHeader"] {
             align-items: flex-start;
             height: auto;
