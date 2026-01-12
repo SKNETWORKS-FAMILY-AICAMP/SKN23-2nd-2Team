dfd75ddd2a04f0491a79399f7928715f50b3da09
JUNGHEEYOUNG9090 <hwarang29@naver.com>
2026-01-12 17:05:25 +0900

    machine_module에 모든 모델 삽입

--- src/modules/machine_module.py
+++ src/modules/machine_module.py
@@ -4,6 +4,8 @@ from sklearn.metrics import (
     accuracy_score, f1_score, precision_score, recall_score, r2_score
 )
 import numpy as np
+from xgboost import XGBClassifier, XGBRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from imblearn.over_sampling import SMOTE
 
 
@@ -183,3 +185,99 @@ def LGBM_module(
     )
 
     return result
+
+# XGBoost 모델
+def XGB_module(
+    df,
+    target_col: str,
+    feature_col=None,
+    drop_cols=None,
+    task="classifier",
+    xgb_params=None,
+    test_size=0.2,
+    random_state=42,
+    stratify=None,
+    threshold=0.5,
+    imbalance=True,
+    smote=True,
+    smote_k_neighbors=5,
+):
+    xgb_params = xgb_params or {
+        "n_estimators": 500,
+        "max_depth": 4,
+        "learning_rate": 0.1,
+        "subsample": 0.8,
+        "colsample_bytree": 0.8,
+    }
+
+    if task == "classifier":
+        model = XGBClassifier(
+            random_state=random_state,
+            eval_metric="logloss",
+            **xgb_params
+        )
+    else:
+        model = XGBRegressor(random_state=random_state, **xgb_params)
+
+    return ML_module(
+        model=model,
+        df=df,
+        target_col=target_col,
+        feature_col=feature_col,
+        drop_cols=drop_cols,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=stratify,
+        threshold=threshold,
+        imbalance=imbalance,
+        smote=smote,
+        smote_k_neighbors=smote_k_neighbors,
+    )
+    
+# 랜덤포레스트 모델
+def RF_module(
+    df,
+    target_col: str,
+    feature_col: list[str] | None = None,
+    drop_cols: list[str] | None = None,
+    task: str = "classifier",
+    rf_params: dict | None = None,
+    test_size: float = 0.2,
+    random_state: int = 42,
+    stratify=None,
+    threshold: float = 0.5,
+    imbalance: bool = False,
+    smote: bool = False,
+    smote_k_neighbors: int = 5,
+):
+    rf_params = rf_params or {}
+    drop_cols = drop_cols or []
+
+    if task == "classifier":
+        model = RandomForestClassifier(
+            random_state=random_state,
+            **rf_params
+        )
+    elif task == "regressor":
+        model = RandomForestRegressor(
+            random_state=random_state,
+            **rf_params
+        )
+    else:
+        raise ValueError('task는 "classifier" 또는 "regressor"')
+
+    return ML_module(
+        model=model,
+        df=df,
+        target_col=target_col,
+        feature_col=feature_col,
+        drop_cols=drop_cols,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=stratify,
+        threshold=threshold,
+        imbalance=imbalance,
+        smote=smote,
+        smote_k_neighbors=smote_k_neighbors,
+        
+    )
