b6f79bb5744089345e0e878fbc5679efb0fb2d72
정유선 <acft9609@gmail.com>
2026-01-15 01:32:31 +0900

    모델 성능 확인 페이지 수정

--- src/views/ModelAnalytics.py
+++ src/views/ModelAnalytics.py
@@ -7,12 +7,12 @@ st.set_page_config(
     page_icon=":material/analytics:",
 )
 
-st.header("모델 성능 확인하기~")
-
 tab1, tab2 = st.tabs(["머신러닝", "딥러닝"])
 
 with tab1:
-    render_machine_learning_tab()
+    with st.container(key="machine_container"):
+        render_machine_learning_tab()
 
 with tab2:
-    render_deep_learning_tab()
+    with st.container(key="deep_container"):
+        render_deep_learning_tab()
